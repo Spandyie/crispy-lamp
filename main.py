e65de49559f00bc915f0842c5fdd9bf8ef560f50
Spandyie <smishra28@gmail.com>
2020-10-23 14:38:53 -0700

    create a separate scarper file that scraoes data from yahoo finance

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import numpy as np
 import altair as alt
 import itertools
 import random
+from scrapeYahoo import load_summary
 
 fmp = FMP(api_key='14b4fa2d27fbcd148244dec7b578caa7')
 
@@ -22,7 +23,7 @@ def download_sp500(num_sample):
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = BeautifulSoup(resp.text)
     table = soup.find('table', {'class': 'wikitable sortable'})
-    num_samples = 100
+    num_samples = num_sample
     tickers = {}
     for row in table.findAll('tr')[1:]:
         row_contents = row.findAll('td')
@@ -34,77 +35,86 @@ def download_sp500(num_sample):
     return tickers
 
 
-def value_to_float(value):
-    """Argument:
-    value : (String) Digits converted to striing
-    """
-    try:
-        if "," in value:
-            value = value.replace(",", "")
-
-        if '$' in value:
-            value = value.replace("$", "")
-
-        if type(value) == float or type(value) == int:
-            return value
-
-        if 'K' in value:
-            if len(value) > 1:
-                return float(value.replace('K', '')) * 1000
-            return 1000
-
-        if 'B' in value:
-            if len(value) > 1:
-                return float(value.replace('B', '')) * 1000000000
-            return 1000000000
-
-        if 'M' in value:
-            if len(value) > 1:
-                return float(value.replace('M', '')) * 1000000
-            return 1000000
-
-        if '%' in value:
-            return round(float(value.replace('%', '')) / 100, 4)
-
-        else:
-            return float(value)
-
-    except:
-        return value
-
-
-@st.cache
-def load_summary(num_files):
-    summary_metrics = list()
-    company_names = download_sp500(num_files)
-    for company, value in company_names.items():
-        data_from_indv_company = summary_data(company)
-        data_from_indv_company['company_name'] = value[0]
-        data_from_indv_company['industry_type'] = value[-1]
-        summary_metrics.append(data_from_indv_company)
-    m_summary_metrics_df = pd.DataFrame(summary_metrics)
-    m_summary_metrics_df.to_csv("summary_data.csv")
-    return m_summary_metrics_df
-
-
-@st.cache
-def summary_data(company_name):
-    resp = requests.get(f'https://financialmodelingprep.com/financial-summary/{company_name}')
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    metric_tables = soup.find("div", {"id", "financial-summary"})
-    summary = {}
-    table_list = metric_tables.findAll('table', attrs={'class': 'table'})
-    for table in table_list:
-        for elements in table.findAll('tr'):
-            key = elements.findAll("th")[0].text
-            value = elements.findAll("td")[0].text
-            try:
-                summary[key] = value_to_float(value)
-            except KeyError:
-                print("Key not found")
-            except ValueError:
-                pass
-    return summary
+# def value_to_float(value):
+#     """Argument:
+#     value : (String) Digits converted to striing
+#     """
+#     try:
+#         if "," in value:
+#             value = value.replace(",", "")
+#
+#         if '$' in value:
+#             value = value.replace("$", "")
+#
+#         if type(value) == float or type(value) == int:
+#             return value
+#
+#         if 'K' in value:
+#             if len(value) > 1:
+#                 return float(value.replace('K', '')) * 1000
+#             return 1000
+#
+#         if 'B' in value:
+#             if len(value) > 1:
+#                 return float(value.replace('B', '')) * 1000000000
+#             return 1000000000
+#
+#         if 'M' in value:
+#             if len(value) > 1:
+#                 return float(value.replace('M', '')) * 1000000
+#             return 1000000
+#
+#         if '%' in value:
+#             return round(float(value.replace('%', '')) / 100, 4)
+#
+#         else:
+#             return float(value)
+#
+#     except:
+#         return value
+#
+#
+# @st.cache
+# def load_summary(num_files):
+#     summary_metrics = list()
+#     company_names = download_sp500(num_files)
+#     for company, value in company_names.items():
+#         data_from_indv_company = summary_data(company)
+#         data_from_indv_company['company_name'] = value[0]
+#         data_from_indv_company['industry_type'] = value[-1]
+#         summary_metrics.append(data_from_indv_company)
+#     m_summary_metrics_df = pd.DataFrame(summary_metrics)
+#     m_summary_metrics_df.to_csv("summary_data.csv")
+#     return m_summary_metrics_df
+#
+#
+# @st.cache
+# def summary_data(company_name):
+#     url = f'https://financialmodelingprep.com/financial-summary/{company_name}'
+#     browser = webdriver.PhantomJS()
+#     browser.get(url)
+#     try:
+#         element = WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.NAME, "financial_summary")))
+#     finally:
+#         browser.quit()
+#     # soup = BeautifulSoup(browser.page_source)
+#     # resp = requests.get(f'https://financialmodelingprep.com/financial-summary/{company_name}')
+#     # soup = BeautifulSoup(resp.text, 'html.parser')
+#     # metric_tables = soup.find("div", {"id", "financial-summary"})
+#     table_list = element.get_attribute('table')
+#     summary = {}
+#     # table_list = metric_tables.findAll('table', attrs={'class': 'table'})
+#     for table in table_list:
+#         for elements in table.findAll('tr'):
+#             key = elements.findAll("th")[0].text
+#             value = elements.findAll("td")[0].text
+#             try:
+#                 summary[key] = value_to_float(value)
+#             except KeyError:
+#                 print("Key not found")
+#             except ValueError:
+#                 pass
+#     return summary
 
 
 if __name__ == "__main__":
@@ -121,18 +131,19 @@ if __name__ == "__main__":
             difference = datetime.datetime.now() - last_modified_date
             if difference.days > 1:
                 data_load_state.text('Loading data...done!')
-                summary_metrics_df = load_summary(50)
+                summary_metrics_df = load_summary(200)
 
             else:
                 # if the file does not need to be modified, just load the existing file
                 summary_metrics_df = st.cache(pd.read_csv)("summary_data.csv")
         except FileNotFoundError:
-            summary_metrics_df = load_summary(50)
+            print(FileNotFoundError)
+            summary_metrics_df = load_summary(200)
     elif s_n_p_file.is_file():
         # cache the data
         summary_metrics_df = st.cache(pd.read_csv)("summary_data.csv")
     else:
-        summary_metrics_df = load_summary(500)
+        summary_metrics_df = load_summary(200)
     data_load_state.text('Loading data...done!')
     ########################################################
     # write the raw data
@@ -144,22 +155,37 @@ if __name__ == "__main__":
     # Allow the users to select the names of the company
     names_of_the_company = st.sidebar.multiselect("Enter names of the company", summary_metrics_df['company_name'].unique())
     st.write("Your selected companies are", summary_metrics_df[summary_metrics_df['company_name']
-             .isin(names_of_the_company)][['Symbol','industry_type']])
+             .isin(names_of_the_company)][['Symbol', 'industry_type']])
     # now select the metrics users want to use
-    metric_list: list = st.selectbox("Which metrics would you want to consider for the evaluation?",
-                                     ("P/E", "Debt / Equity", "ROE", "ROA", "Market Cap", "Price"))
+    # metric_list: list = st.selectbox("Which metrics would you want to consider for the evaluation?",
+    #                                  ("P/E", "Debt / Equity", "ROE", "ROA", "Market Cap", "Price"))
+    metric_list: list = st.multiselect("Which metrics would you want to consider for the evaluation?",
+                                     ("Beta (5Y Monthly)","PE Ratio (TTM)", "EPS (TTM)"))
     st.write('You selected:', metric_list)
     #########################################################
     st.subheader("Value of the shortlisted stocks")
     selected_company_data = summary_metrics_df[summary_metrics_df['company_name'].isin(names_of_the_company)]
-    select_company_data = selected_company_data[['company_name', metric_list]]
-    select_company_data_sorted = select_company_data.sort_values(by=metric_list, ascending=False)
+    select_company_data = selected_company_data[['company_name', 'Previous Close', 'Open','52 Week Range',
+                                                 "Forward Dividend & Yield","Ex-Dividend Date",
+                                                 "Beta (5Y Monthly)","PE Ratio (TTM)", "EPS (TTM)"]]
+    if metric_list:
+        select_company_data_sorted = select_company_data.sort_values(by=metric_list[0], ascending=False)
+    else:
+        select_company_data_sorted = select_company_data
     if st.checkbox('Show raw data from the selected '):
         st.write(select_company_data_sorted)
     #####################################################
-    st.altair_chart(alt.Chart(selected_company_data).mark_bar()
-                    .encode(x=alt.X('company_name', sort=alt.SortField(field=metric_list, order='descending'),
+    if st.checkbox(f"Display the {metric_list[0]} graph") and metric_list:
+        st.altair_chart(alt.Chart(selected_company_data).mark_bar()
+                    .encode(x=alt.X('company_name', sort=alt.SortField(field=metric_list[0], order='descending'),
                                     axis=alt.Axis(title='Company Name')),
-                            y=alt.Y(metric_list + ":Q", axis=alt.Axis(title=metric_list)),
-                            color='Symbol:N')
-                    .interactive(), use_container_width=True)
+                            y=alt.Y(metric_list[0] + ":Q", axis=alt.Axis(title=metric_list[0])),
+                            color='Symbol:N').interactive(), use_container_width=True)
+
+    if len(metric_list)>1 and st.checkbox(f"Display the {metric_list[1]} graph"):
+        st.altair_chart(alt.Chart(selected_company_data).mark_bar()
+                        .encode(x=alt.X('company_name', sort=alt.SortField(field=metric_list[1], order='descending'),
+                        axis=alt.Axis(title='Company Name')),
+                        y=alt.Y(metric_list[1] + ":Q", axis=alt.Axis(title=metric_list[1])),
+                        color='Symbol:N').interactive(), use_container_width=True)
+
---
+++ scrapeYahoo.py
@@ -0,0 +1,111 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+
+import random
+import pandas as pd
+import time
+import requests
+
+def value_to_float(value):
+    """Argument:
+    value : (String) Digits converted to striing
+    """
+    try:
+        if "," in value:
+            value = value.replace(",", "")
+
+        if '$' in value:
+            value = value.replace("$", "")
+
+        if type(value) == float or type(value) == int:
+            return value
+
+        if 'K' in value:
+            if len(value) > 1:
+                return float(value.replace('K', '')) * 1000
+            return 1000
+        if 'T' in value:
+            if len(value) > 1:
+                return float(value.replace('T','')) * 1000000000000
+            return 1000000000000
+
+        if 'B' in value:
+            if len(value) > 1:
+                return float(value.replace('B', '')) * 1000000000
+            return 1000000000
+
+        if 'M' in value:
+            if len(value) > 1:
+                return float(value.replace('M', '')) * 1000000
+            return 1000000
+
+        if '%' in value:
+            return round(float(value.replace('%', '')) / 100, 4)
+
+        else:
+            return float(value)
+
+    except:
+        return value
+
+
+
+def download_sp500(num_sample: int) -> dict:
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = BeautifulSoup(resp.text)
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    num_samples = num_sample
+    tickers = {}
+    for row in table.findAll('tr')[1:]:
+        row_contents = row.findAll('td')
+        name = row_contents[0].text.strip()
+        company_name = row_contents[1].text.strip()
+        industry_type = row_contents[3].text.strip()
+        tickers[name] = (company_name, industry_type)
+    tickers = dict(random.sample(tickers.items(), num_samples))
+    return tickers
+
+
+def scrape_yahoo(ticker):
+    summary = {}
+    summary["Symbol"]=ticker
+    url = f'https://finance.yahoo.com/quote/{ticker}?p={ticker}'
+    x_path_left = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]"
+    x_path_right = '// *[ @ id = "quote-summary"]'
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    browser = webdriver.Chrome('/usr/bin/chromedriver', options=options)
+    browser.implicitly_wait(10)
+    browser.get(url)
+    element = browser.find_element_by_id("quote-summary")
+    rows = element.find_elements_by_tag_name("tr")
+    for row in rows:
+        key = row.find_elements_by_tag_name("td")[0].text
+        value = row.find_elements_by_tag_name("td")[1].text
+        try:
+            summary[key] = value_to_float(value)
+        except ValueError:
+            summary[key] = value
+    return summary
+
+
+def load_summary(num_files) -> object:
+    summary_metrics = list()
+    company_names = download_sp500(num_files)
+    for company, value in company_names.items():
+        data_from_indv_company = scrape_yahoo(company)
+        data_from_indv_company['company_name'] = value[0]
+        data_from_indv_company['industry_type'] = value[-1]
+        summary_metrics.append(data_from_indv_company)
+    m_summary_metrics_df = pd.DataFrame(summary_metrics)
+    m_summary_metrics_df.to_csv("summary_data.csv")
+    return m_summary_metrics_df
+
+
+if __name__ == "__main__":
+    summary = load_summary(10)
+    print(summary)
